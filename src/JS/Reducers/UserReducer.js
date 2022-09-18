f98827e5facf29e1ab5ba2c77aef94ea63b9cd85
Youssef-Dhaou <youssefdhaouu@gmail.com>
2022-09-18 15:52:38 +0100

    deploy post app

--- src/JS/Reducers/UserReducer.js
+++ src/JS/Reducers/UserReducer.js
@@ -2,10 +2,11 @@ const initialState = { users: [] };
 const UserReducer = (state= initialState,{type, payload}) => {
     switch (type) {
         case "": 
+        break;
         
     
         default: 
-        return state
+        return state;
     }
 }
 
